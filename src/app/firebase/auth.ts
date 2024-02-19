e5168f7ca469d74df7ea3f1bf66cad599aef2d2f
innovex <iannjoroge120@outlook.com>
2024-02-19 10:59:11 +0300

    added ablum page ,photos page and phot page

--- src/app/firebase/auth.ts
+++ src/app/firebase/auth.ts
@@ -1,31 +1,34 @@
 import firebase_app from "./config"
-import { createUserWithEmailAndPassword, getAuth,signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth,signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 const auth = getAuth(firebase_app);
 
 
-async function signUp(email : string, password : string) {
-    let result = null,
-        error = null;
+async function signUp(email: string, password: string, displayName: string,gender:string) {
     try {
-        result = await createUserWithEmailAndPassword(auth, email, password);
-    } catch (e) {
-        error = e;
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  
+      // Set the user's display name and photoURL (profile picture)
+      await updateProfile(userCredential.user, {
+        displayName: displayName,
+        photoURL: `https://avatar.iran.liara.run/public/${gender}`,
+      });
+  
+      return { user: userCredential.user, error: null };
+    } catch (error:any) {
+      throw new Error(error.message)
     }
-
-    return { result, error };
-}
+  }
 
 async function signIn(email:string, password :string) {
     let result = null,
         error = null;
     try {
         result = await signInWithEmailAndPassword(auth, email, password);
-    } catch (e) {
-        error = e;
+        return result;
+    } catch (error:any) {
+        throw new Error(error.message)
     }
-
-    return { result, error };
 }
 
 export {signIn,signUp,auth}
